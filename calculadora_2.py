2792b0c81cb14010ba23933f84b289e8570f7c9c
Omar Antonio Lopez Espinoza <oalopeze@alumno.uspg.edu.gt>
2026-02-07 16:02:55 -0600

    mi primer commit

---
+++ calculadora_2.py
@@ -0,0 +1,42 @@
+# Calculadora básica en Python   main()
+
+try:
+    numero_1 = int(input("Ingrese un número: "))
+except ValueError:
+    print("Error: El primer valor ingresado no es un número válido.")
+    exit()
+
+try:
+    numero_2 = int(input("Ingrese otro número: "))
+except ValueError:
+    print("Error: El segundo valor ingresado no es un número válido.")
+    exit()
+
+
+print("seleccione una operación")
+print("Sus opciones son las siguientes ")
+print("Suma: +")
+print("Resta: -")
+print("Multiplicación: *")
+print("Divición: /")
+operacion=input("Seleccione el simbolo de la operación")
+
+if operacion=="+":
+    print(f"Resultado:{numero_1 + numero_2}")
+elif operacion=="-":     
+    print(f"Resultado:{numero_1 - numero_2}")
+elif operacion=="*":
+    print(f"Resultado:{numero_1 * numero_2}")
+elif operacion=="/":
+    
+    if numero_2 == 0:
+        print("Error: No se puede dividir por cero.")
+    else:
+        print(f"Resultado:{numero_1 / numero_2}")
+else:
+     print("La operacion ingresada no es valida")
+
+
+
+
+
---
+++ dibujo.py
@@ -0,0 +1,13 @@
+import turtle
+
+t = turtle.Turtle()
+t.speed(2)
+
+
+
+for i in range(4):
+    t.forward(100)
+    t.left(90)
+
+
+turtle.done()
